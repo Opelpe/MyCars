f05220cddf5ccf06cc07d704f25bca127ec5d768
Opelpe <91432013+Opelpe@users.noreply.github.com>
2023-11-14 22:22:34 +0100

    Added app to manifest

--- app/src/main/java/com/pepe/mycars/app/ui/view/splash/SplashActivity.kt
+++ app/src/main/java/com/pepe/mycars/app/ui/view/splash/SplashActivity.kt
@@ -1,7 +1,6 @@
 package com.pepe.mycars.app.ui.view.splash
 
 import android.os.Bundle
-import android.util.Log
 import com.pepe.mycars.app.MyCarsApp
 import com.pepe.mycars.app.ui.common.BaseActivity
 import com.pepe.mycars.databinding.ActivitySplashBinding
@@ -13,7 +12,7 @@ class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        MyCarsApp().getAppComponent().inject(this)
+        (application as MyCarsApp).getAppComponent().inject(this)
 
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
