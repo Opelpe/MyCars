8714a71ae0835894c2cc0194bbe4585987305c6a
Opelpe <91432013+Opelpe@users.noreply.github.com>
2023-12-17 21:40:41 +0100

    Cleaning files

--- app/src/main/java/com/pepe/mycars/app/data/domain/repository/MyRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.pepe.mycars.app.data.domain.repository
-
-interface MyRepository {
-
-    suspend fun doNetworkCall()
-}
--- app/src/main/java/com/pepe/mycars/app/data/remote/MyApi.kt
+++
@@ -1,8 +0,0 @@
-package com.pepe.mycars.app.data.remote
-
-import retrofit2.http.GET
-
-interface MyApi {
-    @GET("test")
-    suspend fun doNetworkCall()
-}
