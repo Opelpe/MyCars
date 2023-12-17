175bec3f51a67178a5f45192c8d79747347be355
Opelpe <91432013+Opelpe@users.noreply.github.com>
2023-12-17 21:35:24 +0100

    AppModule update

--- app/src/main/java/com/pepe/mycars/app/di/module/AppModule.kt
+++ app/src/main/java/com/pepe/mycars/app/di/module/AppModule.kt
@@ -1,11 +1,14 @@
 package com.pepe.mycars.app.di.module
 
-import com.pepe.mycars.app.data.remote.MyApi
+import android.content.Context
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import com.pepe.mycars.app.utils.SharedPrefConstants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Module
@@ -14,12 +17,20 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun providesMyApi(): MyApi {
-        return Retrofit.Builder()
-            .baseUrl("https://test.com")
-            .build()
-            .create(MyApi::class.java)
+    fun provideContext(@ApplicationContext appContext: Context): Context {
+        return appContext
     }
 
+    @Provides
+    @Singleton
+    fun provideSharedPref(@ApplicationContext context: Context): SharedPreferences {
+        return context.getSharedPreferences(SharedPrefConstants.LOCAL_SHARED_PREF,Context.MODE_PRIVATE)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson {
+        return Gson()
+    }
 
 }
