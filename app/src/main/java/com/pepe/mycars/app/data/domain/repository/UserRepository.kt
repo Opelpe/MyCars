850a5a5626ad62f6ba3b9665e47e7439ad729429
Opelpe <91432013+Opelpe@users.noreply.github.com>
2023-12-17 21:42:59 +0100

    UserRepository update

--- app/src/main/java/com/pepe/mycars/app/data/domain/repository/UserRepository.kt
+++ app/src/main/java/com/pepe/mycars/app/data/domain/repository/UserRepository.kt
@@ -1,11 +1,10 @@
 package com.pepe.mycars.app.data.domain.repository
 
+import com.pepe.mycars.app.data.model.UserModel
+import com.pepe.mycars.app.utils.Resource
+import kotlinx.coroutines.flow.Flow
+
 interface UserRepository {
+    fun getLoggedUserData() : Flow<Resource<UserModel>>
 
-    fun getUserId(): Long
-    fun getUserName(): String
-
-    fun isUserLogged(): Boolean
-
-    fun setUserLogged(isLogged: Boolean)
 }
--- app/src/main/java/com/pepe/mycars/app/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/pepe/mycars/app/data/repository/UserRepositoryImpl.kt
@@ -1,34 +1,85 @@
 package com.pepe.mycars.app.data.repository
 
-import android.app.Application
-import com.pepe.mycars.R
+import android.content.SharedPreferences
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.HttpException
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.gson.Gson
 import com.pepe.mycars.app.data.domain.repository.UserRepository
-import com.pepe.mycars.app.data.remote.MyApi
-import javax.inject.Inject
+import com.pepe.mycars.app.data.model.UserModel
+import com.pepe.mycars.app.utils.FireStoreCollection.USER
+import com.pepe.mycars.app.utils.FireStoreCollection.USER_GUEST
+import com.pepe.mycars.app.utils.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.tasks.await
+import java.io.IOException
+import java.util.Locale
 
-class UserRepositoryImpl @Inject constructor(private val myApi: MyApi,  private val appContext: Application) : UserRepository {
+class UserRepositoryImpl(
+    private val fireStoreDatabase: FirebaseFirestore,
+    val auth: FirebaseAuth,
+    private val appPreferences: SharedPreferences,
+    val gson: Gson,
+) : UserRepository {
 
+    override fun getLoggedUserData(): Flow<Resource<UserModel>> = flow {
+        emit(Resource.Loading())
+        val firebaseUser = auth.currentUser
+        try {
+            if (firebaseUser != null) {
+                val uId = firebaseUser.uid
+                val autoLogin = appPreferences.getBoolean("autoLogin", false)
+                val dbCollection = if (firebaseUser.isAnonymous) USER_GUEST else USER
+                var snapshot = fireStoreDatabase.collection(dbCollection).document(uId).get().await()
+                val providerType = appPreferences.getString("provider", "") ?: ""
 
-    private var isLogged = false
+                if (snapshot.exists()) {
+                    fireStoreDatabase.collection(dbCollection).document(uId).update("autoLogin", autoLogin)
+                        .await()
+                    snapshot = fireStoreDatabase.collection(dbCollection).document(uId).get().await()
+                    val userModel = snapshot.toObject(UserModel::class.java)
 
-    init {
-        val appName = appContext.getString(R.string.app_name)
-        println("UserRepository -> app name is : $appName")
-    }
+                    if (userModel != null) {
+                        if (providerType.isNotEmpty() && userModel.providerType != providerType) {
+                            fireStoreDatabase.collection(dbCollection).document(uId)
+                                .update("providerType", providerType).await()
+                        }
+                        emit(Resource.Success(data = userModel))
+                    } else {
+                        emit(Resource.Error(message = "Unknown error"))
+                    }
 
-    override fun getUserId(): Long {
-        TODO("Not yet implemented")
-    }
-
-    override fun getUserName(): String {
-        TODO("Not yet implemented")
-    }
-
-    override fun isUserLogged(): Boolean {
-        return isLogged
-    }
-
-    override fun setUserLogged(isLogged: Boolean) {
-       this.isLogged = isLogged
+                } else {
+                    val email = firebaseUser.email ?: ""
+                    val country = Locale.getDefault().country
+                    if (dbCollection == USER_GUEST) {
+                        val snapshot = fireStoreDatabase.collection(dbCollection).get().await()
+                        val name = "Guest(" + snapshot.size() + ")"
+                        val userModel = UserModel(name, email, true, country, providerType, uId, autoLogin)
+                        fireStoreDatabase.collection(USER_GUEST).document(uId).set(userModel)
+                            .await()
+                        emit(Resource.Success(data = userModel))
+                    } else {
+                        val name = appPreferences.getString("userName", null) ?: ""
+                        val userModel = UserModel(name, email, true, country, providerType, uId, autoLogin)
+                        fireStoreDatabase.collection(dbCollection).document(uId).set(userModel).await()
+                        emit(Resource.Success(data = userModel))
+                    }
+                }
+            } else {
+                emit(Resource.Error(message = "Not logged", data = null))
+            }
+        } catch (e: HttpException) {
+            emit(Resource.Error(message = e.localizedMessage ?: "Unknown Error"))
+        } catch (e: IOException) {
+            emit(
+                Resource.Error(
+                    message = e.localizedMessage ?: "Check Your Internet Connection"
+                )
+            )
+        } catch (e: Exception) {
+            emit(Resource.Error(message = e.localizedMessage ?: "Unknown exception"))
+        }
     }
 }
