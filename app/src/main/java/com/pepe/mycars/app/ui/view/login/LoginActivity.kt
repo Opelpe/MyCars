84ac07cc1ca0f14bccd226752d0b0a68877982f7
Opelpe <91432013+Opelpe@users.noreply.github.com>
2023-12-17 21:37:04 +0100

    Activity updates

--- app/src/main/java/com/pepe/mycars/app/ui/view/login/LoginActivity.kt
+++ app/src/main/java/com/pepe/mycars/app/ui/view/login/LoginActivity.kt
@@ -2,53 +2,90 @@ package com.pepe.mycars.app.ui.view.login
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.pepe.mycars.app.MyCarsApp
+import com.pepe.mycars.app.ui.view.login.dialog.CreateAccountDialog
+import com.pepe.mycars.app.ui.view.login.dialog.LoginDialog
 import com.pepe.mycars.app.ui.view.main.MainViewActivity
-import com.pepe.mycars.app.ui.viewmodel.login.LoginViewModel
-import com.pepe.mycars.app.ui.viewmodel.splash.SplashViewModel
+import com.pepe.mycars.app.utils.ColorUtils
+import com.pepe.mycars.app.utils.displayToast
+import com.pepe.mycars.app.viewmodel.AuthViewModel
 import com.pepe.mycars.databinding.ActivityLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class LoginActivity: AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private val authModel: AuthViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setButtonsTextColor()
 
-        val vm: LoginViewModel by viewModels()
-        vm.viewState.observe(this){
+        binding.accountLoginButton.setOnClickListener {
+            showLoginDialog()
+        }
+        binding.anonymousLoginButton.setOnClickListener {
+            authModel.registerAsGuest(binding.startCheckBox.isChecked)
+        }
+        binding.createAccountButton.setOnClickListener {
+            showCreateNewAccountDialog()
+        }
 
-            if (it.showToastMessage){
-                Toast.makeText(this, it.toastMessage, Toast.LENGTH_SHORT).show()
+        authModel.authStateModel.observe(this) {
+            setProgressVisibility(it.isLoading)
 
+            if (it.error.isNotBlank()) {
+                setProgressVisibility(false)
+                this@LoginActivity.displayToast(it.error)
             }
 
-            if (it.loggedIn){
+            it.data?.let { user ->
+                Log.d(this.javaClass.name, "User is logged in id: " + user.uid + "  provider: " + user.providerId)
                 startMainViewActivity()
             }
         }
+    }
 
-        binding.localLoginButton.setOnClickListener { v ->
-            vm.onGuestButtonClicked()
+    private fun setButtonsTextColor() {
+        binding.anonymousLoginButton.setTextColor(ColorUtils(applicationContext).getButtonSecondColorStateList())
+        binding.startCheckBox.setTextColor(ColorUtils(applicationContext).getCheckBoxColorStateList())
+        binding.createAccountButton.setTextColor(ColorUtils(applicationContext).getCreateAccountColorStateList())
+    }
+
+    private fun setProgressVisibility(loading: Boolean) {
+        if (loading) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+            )
+            binding.progressView.visibility = View.VISIBLE
+        } else {
+            binding.progressView.visibility = View.GONE
+            window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
         }
+    }
 
+    private fun showLoginDialog() {
+        val loginDialog = LoginDialog.newInstance(binding.startCheckBox.isChecked)
+        loginDialog.show(supportFragmentManager, "dialog")
+    }
+
+    private fun showCreateNewAccountDialog() {
+        val createAccountDialog = CreateAccountDialog.newInstance(binding.startCheckBox.isChecked)
+        createAccountDialog.show(supportFragmentManager, "newAccountDialog")
     }
 
     private fun startMainViewActivity() {
         intent = Intent(this, MainViewActivity::class.java)
         startActivity(intent)
     }
-}
 
-data class LoginViewModelState(
-    val showToastMessage: Boolean = false,
-    val loggedIn: Boolean = false,
-    val toastMessage: String = "",
-)
+
+}
--- app/src/main/java/com/pepe/mycars/app/ui/view/main/MainViewActivity.kt
+++ app/src/main/java/com/pepe/mycars/app/ui/view/main/MainViewActivity.kt
@@ -1,38 +1,92 @@
 package com.pepe.mycars.app.ui.view.main
 
+import android.Manifest.permission.POST_NOTIFICATIONS
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.ktx.messaging
+import com.pepe.mycars.R
 import com.pepe.mycars.app.ui.view.login.LoginActivity
-import com.pepe.mycars.app.ui.viewmodel.main.MainViewModel
-import com.pepe.mycars.app.ui.viewmodel.splash.SplashViewModel
+import com.pepe.mycars.app.utils.displayToast
+import com.pepe.mycars.app.utils.logMessage
+import com.pepe.mycars.app.viewmodel.LoggedInViewModel
 import com.pepe.mycars.databinding.ActivityMainViewBinding
-import com.pepe.mycars.databinding.ActivitySplashBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainViewActivity : AppCompatActivity() {
 
-    private var binding: ActivityMainViewBinding? = null
+    private lateinit var binding: ActivityMainViewBinding
+    private val loggedInViewModel: LoggedInViewModel by viewModels()
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Firebase.messaging.subscribeToTopic("all")
+                .addOnCompleteListener { task ->
+                    var msg = "Subscribed"
+                    if (!task.isSuccessful) {
+                        msg = "Subscribe failed"
+                    }
+                    Log.d("FCM", msg)
+                }
+        } else {
+            displayToast(getString(R.string.notification_disabled_title))
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainViewBinding.inflate(layoutInflater)
-        setContentView(binding!!.root)
+        setContentView(binding.root)
+        setNavigation()
+//        askNotificationPermission()
 
-        val vm: MainViewModel by viewModels()
+        loggedInViewModel.getUserData()
+        loggedInViewModel.userStateModel.observe(this) {
+            if (it!!.isLoading) {
+                logMessage("Loading...")
+            }
 
-        vm.viewState.observe(this){
+            if (it.error.isNotEmpty()) {
+                logMessage(it.error)
+                displayToast(it.error)
+            }
 
-            if (it.loggedIn){
-                Toast.makeText(this, "zalogfowano", Toast.LENGTH_SHORT).show()
-            }else{
-                Toast.makeText(this,"niezalogowano", Toast.LENGTH_SHORT).show()
+            if (it.data != null) {
+                logMessage("user id: " + it.data.id)
+                val provider = it.data.providerType
+                var msg = "Logged as: "
+                msg += if (it.data.email.isNotEmpty()){
+                    it.data.email + " \n $provider"
+                }else{
+                    it.data.name
+                }
+                displayToast(msg)
+            }
+
+            if(!it.isLoggedIn){
+                logMessage("user loggingOut")
                 startLoginActivity()
             }
         }
+    }
+
+    private fun setNavigation() {
+        val navView = binding.navView
+        val navigationHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navigationHost.navController
+        NavigationUI.setupWithNavController(navView, navController)
     }
 
     private fun startLoginActivity() {
@@ -40,10 +94,14 @@ class MainViewActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-}
+    private fun askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, POST_NOTIFICATIONS) !=
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissionLauncher.launch(POST_NOTIFICATIONS)
+            }
+        }
+    }
 
-data class MainViewModelState(
-    val showToastMessage: Boolean = false,
-    val loggedIn: Boolean = true,
-    val toastMessage: String = "",
-)
+}
