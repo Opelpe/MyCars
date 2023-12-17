862be2c6bd23c68a813d86746b526e8e982e7a44
Opelpe <91432013+Opelpe@users.noreply.github.com>
2023-12-17 21:37:36 +0100

    Added login dialogs

---
+++ app/src/main/java/com/pepe/mycars/app/ui/view/login/dialog/CreateAccountDialog.kt
@@ -0,0 +1,184 @@
+package com.pepe.mycars.app.ui.view.login.dialog
+
+import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.text.method.TransformationMethod
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.coroutineScope
+import com.pepe.mycars.app.utils.ColorUtils
+import com.pepe.mycars.app.viewmodel.AuthViewModel
+import com.pepe.mycars.databinding.DialogCreateAccountBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class CreateAccountDialog: DialogFragment() {
+
+    private lateinit var binding: DialogCreateAccountBinding
+
+    private val authModel: AuthViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DialogCreateAccountBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+
+    companion object {
+        fun newInstance(autoLogin: Boolean): CreateAccountDialog {
+            val f = CreateAccountDialog()
+            val args = Bundle()
+            args.putBoolean("autoLogin", autoLogin)
+            f.arguments = args
+            return f
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.createUserButton.setOnClickListener {
+            val email = binding.emailEditText.getText().toString()
+            val password = binding.passwordEditText.getText().toString()
+            val name = binding.userNameEditText.getText().toString()
+            createNewUserClicked(email, password, name)
+        }
+
+        binding.cancelButtonText.setTextColor(ColorUtils(requireContext()).getButtonPrimeColorStateList())
+        binding.cancelButton.setOnClickListener {
+            dismiss()
+        }
+
+        binding.passwordVisibilityIcon.imageTintList = ColorUtils(requireContext()).getButtonSecondColorStateList()
+        binding.passwordVisibilityIcon.setOnClickListener {
+            setPasswordVisibility()
+        }
+
+        binding.userNameEditText.onFocusChangeListener =
+            View.OnFocusChangeListener { _: View?, b: Boolean ->
+                if (b) {
+                    binding.userNameTitle.setTextColor(
+                        resources.getColor(
+                            com.pepe.mycars.R.color.green,
+                            activity?.theme
+                        )
+                    )
+                } else {
+                    binding.userNameTitle.setTextColor(
+                        resources.getColor(
+                            com.pepe.mycars.R.color.grey,
+                            activity?.theme
+                        )
+                    )
+                }
+            }
+
+        binding.emailEditText.onFocusChangeListener =
+            View.OnFocusChangeListener { _: View?, b: Boolean ->
+                if (b) {
+                    binding.emailAddressTitle.setTextColor(
+                        resources.getColor(
+                            com.pepe.mycars.R.color.green,
+                            activity?.theme
+                        )
+                    )
+                } else {
+                    binding.emailAddressTitle.setTextColor(
+                        resources.getColor(
+                            com.pepe.mycars.R.color.grey,
+                            activity?.theme
+                        )
+                    )
+                }
+            }
+
+        binding.passwordEditText.onFocusChangeListener =
+            View.OnFocusChangeListener { _: View?, b: Boolean ->
+                if (b) {
+                    binding.passwordTitle.setTextColor(
+                        resources.getColor(
+                            com.pepe.mycars.R.color.green,
+                            activity?.theme
+                        )
+                    )
+                } else {
+                    binding.passwordTitle.setTextColor(
+                        resources.getColor(
+                            com.pepe.mycars.R.color.grey,
+                            activity?.theme
+                        )
+                    )
+                }
+            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        authModel.authStateModel.observe(this){
+            setProgressVisibility(it.isLoading)
+
+            if (it.error.isNotBlank()) {
+                setProgressVisibility(false)
+            }
+            it.data?.let {
+                setProgressVisibility(false)
+            }
+        }
+    }
+
+    private fun setPasswordVisibility() {
+        val transformationMethod: TransformationMethod =
+            binding.passwordEditText.getTransformationMethod()
+        if (transformationMethod == PasswordTransformationMethod.getInstance()) {
+            transformationHide()
+        }
+        if (transformationMethod == HideReturnsTransformationMethod.getInstance()) {
+            transformationShow()
+        }
+    }
+
+    private fun transformationHide() {
+        val passwordInput = binding.passwordEditText
+        val hide = HideReturnsTransformationMethod.getInstance()
+        binding.passwordVisibilityIcon.setImageResource(com.pepe.mycars.R.drawable.ic_password_visibility_off)
+        passwordInput.transformationMethod = hide
+        passwordInput.setSelection(passwordInput.getText().length)
+    }
+
+    private fun transformationShow() {
+        val passwordInput = binding.passwordEditText
+        val show = PasswordTransformationMethod.getInstance()
+        binding.passwordVisibilityIcon.setImageResource(com.pepe.mycars.R.drawable.ic_password_visibility)
+        passwordInput.transformationMethod = show
+        passwordInput.setSelection(passwordInput.getText().length)
+    }
+
+    private fun createNewUserClicked(email: String?, password: String?, name: String?) {
+        val autoLogin = arguments?.getBoolean("autoLogin") ?: false
+        authModel.register(email, password, name, autoLogin)
+    }
+
+    private fun setProgressVisibility(loading: Boolean) {
+        if (loading) {
+            dialog!!.window!!.setFlags(
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+            )
+            binding.progressView.visibility = View.VISIBLE
+        } else {
+            binding.progressView.visibility = View.GONE
+            dialog!!.window!!.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+        }
+    }
+}
---
+++ app/src/main/java/com/pepe/mycars/app/ui/view/login/dialog/LoginDialog.kt
@@ -0,0 +1,181 @@
+package com.pepe.mycars.app.ui.view.login.dialog
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.text.method.TransformationMethod
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.coroutineScope
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.GoogleAuthProvider
+import com.pepe.mycars.app.utils.ColorUtils
+import com.pepe.mycars.app.viewmodel.AuthViewModel
+import com.pepe.mycars.databinding.DialogLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+
+@AndroidEntryPoint
+class LoginDialog : DialogFragment() {
+
+    private lateinit var binding: DialogLoginBinding
+    private lateinit var launcher: ActivityResultLauncher<Intent>
+    private val authModel: AuthViewModel by activityViewModels()
+
+    companion object {
+        fun newInstance(autoLogin: Boolean): LoginDialog {
+            val dialog = LoginDialog()
+            val args = Bundle()
+            args.putBoolean("autoLogin", autoLogin)
+            dialog.arguments = args
+            return dialog
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DialogLoginBinding.inflate(layoutInflater, container, false)
+
+        launcher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val data: Intent? = result.data
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                    signInWithGoogle(task)
+                }
+            }
+
+        dialog?.let {
+            it.window?.requestFeature(Window.FEATURE_NO_TITLE)
+        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.passwordVisibilityIcon.imageTintList =
+            ColorUtils(requireContext()).getButtonSecondColorStateList()
+        binding.passwordVisibilityIcon.setOnClickListener {
+            setPasswordVisibility()
+        }
+
+        binding.submitButton.setOnClickListener {
+            val email = binding.emailInput.getText().toString()
+            val password = binding.passwordInput.getText().toString()
+            onSubmitClicked(email, password)
+        }
+
+        binding.googleLoginButtonText.setTextColor(ColorUtils(requireContext()).getButtonSecondColorStateList())
+        binding.googleLoginButton.setOnClickListener {
+            onSignInWithGoogleClicked()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog ?: return
+        val width = ViewGroup.LayoutParams.MATCH_PARENT
+        val height = ViewGroup.LayoutParams.WRAP_CONTENT
+        dialog?.window?.setLayout(width, height)
+
+        authModel.authStateModel.observe(this){
+            setProgressVisibility(it.isLoading)
+
+            if (it.error.isNotBlank()) {
+                setProgressVisibility(false)
+            }
+            it.data?.let {
+                setProgressVisibility(false)
+            }
+        }
+
+    }
+
+    private fun onSubmitClicked(email: String?, password: String?) {
+        val autoLogin = arguments?.getBoolean("autoLogin") ?: false
+        authModel.login(email, password, autoLogin)
+    }
+
+    private fun onSignInWithGoogleClicked() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(requireContext().getString(com.firebase.ui.auth.R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+        googleSignInClient.signOut()
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private fun signInWithGoogle(task: Task<GoogleSignInAccount>) {
+        val result = task.result
+        if (result != null) {
+            val authCredential = GoogleAuthProvider.getCredential(result.idToken, null)
+            val name = result.displayName ?: ""
+            val email = result.email ?: ""
+            val autoLogin = arguments?.getBoolean("autoLogin") ?: false
+
+            authModel.signInWithGoogle(authCredential, name, email, autoLogin)
+        }
+    }
+
+    private fun setProgressVisibility(loading: Boolean) {
+        if (loading) {
+            dialog!!.window!!.setFlags(
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+            )
+            binding.progressView.visibility = View.VISIBLE
+        } else {
+            binding.progressView.visibility = View.GONE
+            dialog!!.window!!.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+        }
+    }
+
+    private fun setPasswordVisibility() {
+        val transformationMethod: TransformationMethod =
+            binding.passwordInput.getTransformationMethod()
+        if (transformationMethod == PasswordTransformationMethod.getInstance()) {
+            transformationHide()
+        }
+        if (transformationMethod == HideReturnsTransformationMethod.getInstance()) {
+            transformationShow()
+        }
+    }
+
+    private fun transformationHide() {
+        val passwordInput = binding.passwordInput
+        val hide = HideReturnsTransformationMethod.getInstance()
+        binding.passwordVisibilityIcon.setImageResource(com.pepe.mycars.R.drawable.ic_password_visibility_off)
+        passwordInput.transformationMethod = hide
+        passwordInput.setSelection(passwordInput.getText().length)
+    }
+
+    private fun transformationShow() {
+        val passwordInput = binding.passwordInput
+        val show = PasswordTransformationMethod.getInstance()
+        binding.passwordVisibilityIcon.setImageResource(com.pepe.mycars.R.drawable.ic_password_visibility)
+        passwordInput.transformationMethod = show
+        passwordInput.setSelection(passwordInput.getText().length)
+    }
+
+}
