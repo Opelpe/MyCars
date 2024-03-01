691d9e63c8bfaf95420c6e4f0039f42ce421409b
Opelpe <91432013+Opelpe@users.noreply.github.com>
2024-03-01 23:22:47 +0100

    Added view to SettingsFragment in progress

--- app/src/main/java/com/pepe/mycars/app/ui/view/fragment/SettingsFragment.kt
+++ app/src/main/java/com/pepe/mycars/app/ui/view/fragment/SettingsFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.pepe.mycars.R
 import com.pepe.mycars.app.viewmodel.LoggedInViewModel
 import com.pepe.mycars.databinding.FragmentSettingsBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,6 +31,18 @@ class SettingsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.logoutButton.setOnClickListener {
             loggedInViewModel.logOut()
+        }
+
+        binding.languagesContainer.setOnClickListener {
+            if (binding.languagesSetContainer.isVisible){
+                val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.ic_arrow_forward)
+                binding.languagesSetContainer.visibility = View.GONE
+                binding.languagesTitle.setCompoundDrawablesRelativeWithIntrinsicBounds(drawable,null,null,null)
+            }else{
+                val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.ic_double_arrow_down)
+                binding.languagesSetContainer.visibility = View.VISIBLE
+                binding.languagesTitle.setCompoundDrawablesRelativeWithIntrinsicBounds(drawable,null,null,null)
+            }
         }
     }
 }
