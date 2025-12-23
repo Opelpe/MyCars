8b956823d74a5f34c07b36c3e23e11bd6d2101be
Opelpe <91432013+Opelpe@users.noreply.github.com>
2025-12-23 22:37:04 +0100

    FIx gradle: add pickFirsts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,8 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
+            pickFirsts += "META-INF/LICENSE.md"
+            pickFirsts += "META-INF/LICENSE-notice.md"        }
     }
 }
 
